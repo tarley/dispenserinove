3f96d77936cbc237f33bdeb7533580c2bdc761ff
crisoares <cris84bh@gmail.com>
2016-05-24 11:48:34 -0300

    Crud Produto
    Crud para buscar os produtos no BD

--- Source/classes/CrudProduto.php
+++ Source/classes/CrudProduto.php
@@ -1,34 +1,27 @@
 <?php
 require_once 'BDConnection.php';
+
 class CrudProduto {
+	
 	private $db;
-	function __construct() {
-		$conn = new BDConnection ();
-		$this->db = $conn->getConnection ();
-	}
-	private function valida_duplicidade($codigo) {
-		$sql = "select cod_produto from produto where cod_produto = :codigo";
-		$sth = $this->db->prepare ( $sql );
-		$sth->bindValue ( ':codigo', $codigo );
-		
-		$sth->execute ();
-		
-		return $sth->rowCount();
+	
+	function __construct()
+	{
+		$conn = new BDConnection();
+		$this->db = $conn->getConnection();
 	}
 	
-	public function insert($codigo, $descricao) {
-		try {
-			if ($this->valida_duplicidade ( $codigo ) == 0) {
-				$stmt = $this->db->prepare ( "insert into produto (cod_produto, des_produto) VALUES (:codigo, :des)" );
-				$stmt->bindParam ( ':codigo', $codigo );
-				$stmt->bindParam ( ':des', $descricao );
-				
-				return $stmt->execute () ? true : false;
-			} else {
-				return false;
-			}
-		} catch ( PDOException $ex ) {
-			return false;
-		}
+	public function getByFilter($codProduto){
+		
+		$sql .= "select p.cod_produto, p.des_produto";
+		$sql .= "  from produto p ";
+		$sql .= "where p.cod_produto= :codProduto" ;
+		$sth = $this->db->prepare($sql);
+		$sth->bindValue(':codProduto', $codProduto);
+		$sth->execute();
+		
+		return $sth->fetchAll(PDO::FETCH_ASSOC);
 	}
+	
+	
 }
