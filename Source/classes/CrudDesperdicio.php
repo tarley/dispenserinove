ddfdd9e16ad2f080e4666bdf4c0aa85c984308bf
Igorlgwow <igor_luiz_14@hotmail.com>
2016-05-29 15:00:10 -0300

    Add files via upload

---
+++ Source/classes/CrudDesperdicio.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'BDConnection.php';
+
+class CrudDesperdicio {
+	
+	private $db;
+	
+	function __construct()
+	{
+		$conn = new BDConnection();
+		$this->db = $conn->getConnection();
+	}
+	
+	public function desperdicio($cod_retorno, $cod_retirada, $dta_retorno, $num_quant_retorno, $des_motivo) {
+		
+		$sql = "INSERT INTO historico_retorno (cod_retorno,cod_retirada,dta_retorno,num_quant_retorno,des_motivo) VALUES (:codretirada,:data,:quantidade,:motivo);";
+			
+	}
+}	
