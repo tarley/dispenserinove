f0535adb6cc2aa788c7d7d9689abf04daad23b9b
Victor Britto e Barros <victordebritto@yahoo.com.br>
2016-05-31 00:50:52 -0300

    Script de carga basica.

---
+++ Database/CargaBasica.sql
@@ -0,0 +1,2 @@
+insert into funcionario_farmacia (NOM_FUNC, DES_TURNO, COD_SENHA, NOM_USUARIO, DTA_CADASTRO, ADMIN) 
+values ('Administrador', 'N', 'e10adc3949ba59abbe56e057f20f883e', 'admin', '2016-05-31', 1);
