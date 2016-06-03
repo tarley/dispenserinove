71f86871f7817aed097f3c2cac54c95ffc727c1d
Igorlgwow <igor_luiz_14@hotmail.com>
2016-06-03 09:04:30 -0300

    Retirado insert do código autoincrement

--- Source/classes/CrudDesperdicio.php
+++ Source/classes/CrudDesperdicio.php
@@ -11,9 +11,9 @@ class CrudDesperdicio {
 		$this->db = $conn->getConnection();
 	}
 	
-	public function desperdicio($cod_retorno, $cod_retirada, $dta_retorno, $num_quant_retorno, $des_motivo) {
+	public function desperdicio($cod_retirada, $dta_retorno, $num_quant_retorno, $des_motivo) {
 		
-		$sql = "INSERT INTO historico_retorno (cod_retorno,cod_retirada,dta_retorno,num_quant_retorno,des_motivo) VALUES (:codretirada,:data,:quantidade,:motivo);";
+		$sql = "INSERT INTO historico_retorno (cod_retirada,dta_retorno,num_quant_retorno,des_motivo) VALUES (:codretirada,:data,:quantidade,:motivo);";
 			
 	}
 }	
