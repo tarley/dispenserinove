a09554342de1f0ef244b9d387920aa9e9b48cc7d
Victor Britto e Barros <victordebritto@yahoo.com.br>
2016-06-07 04:01:40 -0300

    Gravar desperdicio, mudança no update

--- Source/RegistroDesperdicio.php
+++ Source/RegistroDesperdicio.php
@@ -1,83 +1,91 @@
 <?php
 ob_start ();
-$cod_retirada= null;
+$cod_retirada = null;
 
-if(isset( $_GET['cod_retirada'] )){
-	$cod_retirada=$_GET['cod_retirada'];
+if (isset ( $_GET ['cod_retirada'] )) {
+	$cod_retirada = $_GET ['cod_retirada'];
 }
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		
-		require_once 'classes/CrudDesperdicio.php';
-		require_once 'classes/Util.php';
-		$u = new Util();
-		$crud = new CrudDesperdicio();
-		
-                if($crud->retornoExiste($_POST["codretirada"])) {
-                    if($crud->insertDesp($_POST["codretirada"], $_POST["quantidade"], $_POST["motivo"])){
-                            $u->alerta("Desperdício registrado com sucesso!");
-							$crud->alteraStatus("codretirada");
-                    }else{
-                        $u->alerta("Erro ao tentar registrar desperdício!");
-					}
-				}else{
-					$u->alerta("Desperdício já registrado!");
-				}
-        }
+if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 	
+	require_once 'classes/CrudDesperdicio.php';
+	require_once 'classes/Util.php';
+	$u = new Util ();
+	$crud = new CrudDesperdicio ();
+	
+	if ($crud->retornoExiste ( $_POST ["codretirada"] )) {
+		if ($crud->insertDesp ( $_POST ["codretirada"], $_POST ["quantidade"], $_POST ["motivo"] )) {
+			$crud->alteraStatus ( $_POST ["codretirada"] );
+			$u->alerta ( "Desperdício registrado com sucesso!" );
+		} else {
+			$u->alerta ( "Erro ao tentar registrar desperdício!" );
+		}
+	} else {
+		$u->alerta ( "Desperdício já registrado!" );
+	}
+}
+
 ?>
 
 
 <div class="panel panel-default">
 	<div class="panel-heading">Registro de Desperd&iacute;cio</div>
 	<div class="panel-body">
-		<form method="post" class="form-horizontal" id= "frmDesperdicio">
+		<form method="post" class="form-horizontal" id="frmDesperdicio">
 			<div class="row">
 				<div class="col-md-12">
-					
+
 
 					<div class="form-group"></div>
 
 				</div>
 				<div>
-							
-						<div class="panel-body">
-							<form method="post" class="form-horizontal">
-								<div class="row">
-									<div class="col-md-6">
-				
-										<div class="form-group">
-											<label class="col-sm-3 control-label">C&oacute;digo de retirada do produto</label>
-											<div class="col-sm-9">
-												<input required="true" value="<?php echo $cod_retirada; ?>" type="number" class="form-control" placeholder="C&oacute;digo de retirada" name="codretirada" id="codretirada" min="1">
-											</div>
+
+					<div class="panel-body">
+						<form method="post" class="form-horizontal">
+							<div class="row">
+								<div class="col-md-6">
+
+									<div class="form-group">
+										<label class="col-sm-3 control-label">C&oacute;digo de
+											retirada do produto</label>
+										<div class="col-sm-9">
+											<input required="true" value="<?php echo $cod_retirada; ?>"
+												type="number" class="form-control"
+												placeholder="C&oacute;digo de retirada" name="codretirada"
+												id="codretirada" min="1">
 										</div>
-												
-										<div class="form-group">
-											<label class="col-sm-3 control-label">Quantidade desperdi&ccedil;ada</label>
-											<div class="col-sm-9">
-												<input required="true" type="number" class="form-control" placeholder="Quantidade" name="quantidade" id="quantidade" min="1">
-											</div>
-										</div>
-												
-										<div class="form-group">
-											<label class="col-sm-3 control-label">Motivo</label>
-											<div class="col-sm-9">
-												<textarea  required="true" class="form-control" rows="3" placeholder="Descreva o motivo do desperd&iacute;cio" name="motivo"
-												id="motivo"></textarea>
-											</div>
-										</div>
-												
 									</div>
-											
-									<div class="col-md-12 col-sm-offset-1">
-										<div class="hr-dashed"></div>
-										<button class="btn btn-default" type="reset">Limpar</button>
-										<button class="btn btn-primary" type="submit">Salvar</button>
+
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Quantidade
+											desperdi&ccedil;ada</label>
+										<div class="col-sm-9">
+											<input required="true" type="number" class="form-control"
+												placeholder="Quantidade" name="quantidade" id="quantidade"
+												min="1">
+										</div>
 									</div>
+
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Motivo</label>
+										<div class="col-sm-9">
+											<textarea required="true" class="form-control" rows="3"
+												placeholder="Descreva o motivo do desperd&iacute;cio"
+												name="motivo" id="motivo"></textarea>
+										</div>
+									</div>
+
 								</div>
-							</form>
-						</div>	
-							
+
+								<div class="col-md-12 col-sm-offset-1">
+									<div class="hr-dashed"></div>
+									<button class="btn btn-default" type="reset">Limpar</button>
+									<button class="btn btn-primary" type="submit">Salvar</button>
+								</div>
+							</div>
+						</form>
+					</div>
+
 				</div>
 			</div>
 		</form>
--- Source/classes/CrudDesperdicio.php
+++ Source/classes/CrudDesperdicio.php
@@ -1,79 +1,57 @@
 <?php
 require_once 'BDConnection.php';
-
 class CrudDesperdicio {
-	
 	private $db;
-	
-	function __construct()
-	{
-		$conn = new BDConnection();
-		$this->db = $conn->getConnection();
+	function __construct() {
+		$conn = new BDConnection ();
+		$this->db = $conn->getConnection ();
 	}
-	
-	
 	public function insertDesp($codretirada, $quantidade, $motivo) {
 		try {
-                        
-
+			
 			$sql = "INSERT INTO historico_retorno ";
 			$sql .= "(cod_retirada, dta_retorno, num_quant_retorno, des_motivo) ";
 			$sql .= "VALUES (:codretirada,current_date(), :quantidade,:motivo) ";
-							
-			$stmt = $this->db->prepare($sql);
-				
-			$stmt->bindparam(":codretirada", $codretirada);
-			$stmt->bindparam(":quantidade", $quantidade);
-			$stmt->bindparam(":motivo", $motivo);
-				
-				
+			
+			$stmt = $this->db->prepare ( $sql );
+			
+			$stmt->bindparam ( ":codretirada", $codretirada );
+			$stmt->bindparam ( ":quantidade", $quantidade );
+			$stmt->bindparam ( ":motivo", $motivo );
+			
 			return $stmt->execute () ? true : false;
 		} catch ( PDOException $e ) {
 			return false;
 		}
 	}
-	
-
-	public function retornoExiste($codretirada){
-		try
-		{
-			$stmt = $this->db->prepare("select cod_retirada from historico_retorno where cod_retirada= :codretirada LIMIT 1");
-			$stmt->execute(array(":codretirada"=>$codretirada));
-			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-			if($stmt->rowCount() > 0)
-			{
+	public function retornoExiste($codretirada) {
+		try {
+			$stmt = $this->db->prepare ( "select cod_retirada from historico_retorno where cod_retirada= :codretirada LIMIT 1" );
+			$stmt->execute ( array (
+					":codretirada" => $codretirada 
+			) );
+			$userRow = $stmt->fetch ( PDO::FETCH_ASSOC );
+			if ($stmt->rowCount () > 0) {
 				return false;
-			}
-			else
-			{
+			} else {
 				return true;
 			}
-		}
-		catch(PDOException $e)
-		{
-			return  false;
+		} catch ( PDOException $e ) {
+			return false;
 		}
 	}
-	
-        public function alteraStatus($codretirada){
-            try
-            {
-                
-                    $sql = "UPDATE  produto_retirado";
-                    $sql .= "SET cod_status = '3'";
-                    $sql .= "WHERE cod_retirada = :codretirada" ;
-					
-					$stmt = $this->db->prepare($sql);
-					$stmt->bindparam(":codretirada", $codretirada);				
-				
-					return $stmt->execute () ? true : false;
-
-                }
-                catch(PDOException $e)
-		{
-			return  false;
+	public function alteraStatus($codretirada) {
+		try {
+			$sql = "UPDATE  produto_retirado ";
+			$sql .= "SET cod_status = (select cod_status from situacao_produto where des_status='desperdício' limit 1) ";
+			$sql .= "WHERE cod_retirada = :codretirada";
+			
+			$stmt = $this->db->prepare ( $sql );
+			$stmt->bindparam ( ":codretirada", $codretirada );
+			
+			return $stmt->execute () ? true : false;
+		} catch ( PDOException $e ) {
+			return false;
 		}
-        }
-        
-
+	}
 }	
