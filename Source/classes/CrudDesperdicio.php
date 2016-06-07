c60a0681c3bf447b31120362e4c5d4e361f427e0
Igorlgwow <igor_luiz_14@hotmail.com>
2016-06-07 01:10:48 -0300

    Ajustes no Insert e exceções

--- Source/classes/CrudDesperdicio.php
+++ Source/classes/CrudDesperdicio.php
@@ -12,17 +12,17 @@ class CrudDesperdicio {
 	}
 	
 	
-	public function insertDesp($codretirada, $data, $quantidade, $motivo) {
+	public function insertDesp($codretirada, $quantidade, $motivo) {
 		try {
-			
-			$sql .= "INSERT INTO historico_retorno ";
-			$sql .= "(cod_retirada,dta_retorno,num_quant_retorno,des_motivo) ";
-			$sql .= "VALUES (:codretirada,:data,:quantidade,:motivo) ";
+                        
+
+			$sql = "INSERT INTO historico_retorno ";
+			$sql .= "(cod_retirada, dta_retorno, num_quant_retorno, des_motivo) ";
+			$sql .= "VALUES (:codretirada,current_date(), :quantidade,:motivo) ";
 							
 			$stmt = $this->db->prepare($sql);
 				
 			$stmt->bindparam(":codretirada", $codretirada);
-			$stmt->bindparam(":data", $data);
 			$stmt->bindparam(":quantidade", $quantidade);
 			$stmt->bindparam(":motivo", $motivo);
 				
@@ -33,7 +33,47 @@ class CrudDesperdicio {
 		}
 	}
 	
+
+	public function retornoExiste($codretirada){
+		try
+		{
+			$stmt = $this->db->prepare("select cod_retirada from historico_retorno where cod_retirada= :codretirada LIMIT 1");
+			$stmt->execute(array(":codretirada"=>$codretirada));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($stmt->rowCount() > 0)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		catch(PDOException $e)
+		{
+			return  false;
+		}
+	}
 	
-	
-	
+        public function alteraStatus($codretirada){
+            try
+            {
+                
+                    $sql = "UPDATE  produto_retirado";
+                    $sql .= "SET cod_status = '3'";
+                    $sql .= "WHERE cod_retirada = :codretirada" ;
+					
+					$stmt = $this->db->prepare($sql);
+					$stmt->bindparam(":codretirada", $codretirada);				
+				
+					return $stmt->execute () ? true : false;
+
+                }
+                catch(PDOException $e)
+		{
+			return  false;
+		}
+        }
+        
+
 }	
